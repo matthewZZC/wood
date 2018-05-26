6a499ad339063f740a1a3bf61e1d6229b8b41ef8
zhengzhicheng1 <zhengzhicheng@imdada.cn>
2018-05-26 18:55:23 +0800

    商家首页

--- pages/vender/index.js
+++ pages/vender/index.js
@@ -5,62 +5,85 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    screen_width: wx.getSystemInfoSync().windowWidth,
+    top_image_src: "https://developers.weixin.qq.com/miniprogram/dev/image/cat/0.jpg?t=2018518",
+    top_image_width: 200,
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
+    circular: true,
+    vertical: false,
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    skuList: [{
+      skuName:"图片",
+      skuUrl:"http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"
+    }, {
+        skuName: "图片",
+        skuUrl: "http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"
+    }, {
+        skuName: "图片",
+        skuUrl: "http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log(wx.getSystemInfoSync().windowWidth)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
